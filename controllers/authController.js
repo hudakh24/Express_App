230c6357174ad4cc33d9683f62f91f39432b321c
hudakh24 <hudashoaib2000@gmail.com>
2024-08-14 20:03:41 +0500

    express app

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,39 +2,6 @@ const { compare } = require("bcryptjs");
 const { user } = require("./userController"); // Import users from userController
 
 module.exports = {
-  // login: async (req, res) => {
-  //   try {
-  //     let { username, password } = req.query;
-  //     let userLogin = false;
-
-  //     //The map function itself does not handle promises or async operations so use promise.all
-  //     await Promise.all(
-  //       user.map(async (user) => {
-  //         if (user.username === username) {
-  //           const isMatch = await compare(password, user.password);
-  //           if (isMatch) {
-  //             userLogin = true; // Set flag to true
-  //           }
-  //         }
-  //       })
-  //     );
-
-  //     if (userLogin) {
-  //       return res.send({
-  //         response: `User ${username} loggedIn successfully`,
-  //       });
-  //     } else {
-  //       return res.send({
-  //         response: "User not found or password incorrect",
-  //       });
-  //     }
-  //   } catch (error) {
-  //     return res.send({
-  //       error: error.message,
-  //     });
-  //   }
-  // },
-
   login: async (req, res) => {
     try {
       const { username, password } = req.body;
